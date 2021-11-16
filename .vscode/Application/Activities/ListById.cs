77803d8e101052337b51a80f4dd27fab83a96b66
luisaguirrestrepo <luisaguirre17384@gmail.com>
2021-11-16 17:56:43 -0500

    actualizacion final

---
+++ .vscode/Application/Activities/ListById.cs
@@ -0,0 +1,38 @@
+using System;
+using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Domain;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Persistence;
+
+namespace Application.Activities
+{
+    public class ListById{
+        public class QueryId : IRequest<Activity>{
+            public Guid _guid;
+            
+            public QueryId(Guid guid){
+                _guid = guid;
+                
+            }
+
+
+           
+        }
+    
+    public class Handler : IRequestHandler<QueryId, Activity>
+        {
+             private readonly DataContext _context;
+            public Handler(DataContext context){
+                _context = context;
+            }
+            public async Task<Activity> Handle(QueryId request, CancellationToken cancellationToken){
+                return await _context.Activities.FindAsync(request._guid);
+            }
+
+    }
+    }
+       
+}
